ec54535ea659ad2b8c3c772dba380607d4896320
sherv13-tech <shervinlota76@gmail.com>
2025-02-09 15:14:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let messageIndex = 0;
+const messages = ["Are you sure?", "Think again!", "Really?", "Last chance!", "Please 🥺"];
+
+function handleNoClick() {
+    const noButton = document.querySelector(".no-button");
+    const yesButton = document.querySelector(".yes-button");
+
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
